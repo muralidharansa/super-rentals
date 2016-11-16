2b02ee94d03e9e14a7fd377cf5cd41d8faea84f2
muralidharan <muralidharan.sa@gmail.com>
2016-11-16 11:04:30 +0530

    my updates
    My Updated files

---
+++ tests/acceptance/list-rentals-test.js
@@ -0,0 +1,16 @@
+import Ember from 'ember';
+import moduleForAcceptance from 'super-rentals/tests/helpers/module-for-acceptance';
+
+
+let StubMapsService = Ember.Service.extend({
+  getMapElement() {
+    return document.createElement('div');
+  }
+});
+
+moduleForAcceptance('Acceptance | list rentals', {
+  beforeEach() {
+    this.application.register('service:stubMaps', StubMapsService);
+    this.application.inject('component:location-map', 'maps', 'service:stubMaps');
+  }
+});
